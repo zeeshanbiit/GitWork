92570ba3122536b45433d0eb35ff33fa25884103
Zeeshan <zeeshanbiit786@gmail.com>
2018-01-17 12:56:48 +0500

    Add Map Feature branch

---
+++ GitWork/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitWork
+//
+//  Created by Zeeshan on 17/01/2018.
+//  Copyright © 2018 Zeeshan. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
