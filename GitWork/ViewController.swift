424e875077f37cd885911f534583642eaa71295e
Zeeshan <zeeshanbiit786@gmail.com>
2018-01-17 12:18:39 +0500

    Initial Commit

---
+++ GitWork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitWork
+//
+//  Created by Zeeshan on 17/01/2018.
+//  Copyright © 2018 Zeeshan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
