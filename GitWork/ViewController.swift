fdd5239377dae4264a6adeb755746836137d82e5
Zeeshan <zeeshanbiit786@gmail.com>
2018-01-17 16:05:31 +0500

    Finished Work For Today

--- GitWork/ViewController.swift
+++ GitWork/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func buttonClicked(_ sender: UIButton) {
-        print("This is a Git tutorial")
+        print("You Finished")
     }
 
     
